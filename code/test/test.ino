0484804ace4655ef9cb3946361bca29f136cea19
22236bhs <22236@burnside.school.nz>
2025-07-18 09:26:22 +1200

    added code to test if esp32 can power led

--- code/test/test.ino
+++ code/test/test.ino
@@ -1,20 +1,23 @@
-int pin = 36;
+int button_pin = 36;
+int led_pin = 34;
 
 
 void setup(){
   Serial.begin(9600);
-  pinMode(pin, INPUT);
+  pinMode(button_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
 }
  
 void loop(){
+  setLED(readButton());
   Serial.println(readButton());
-  
 }
 
 bool readButton(){
-  return digitalRead(pin);
+  return digitalRead(button_pin);
 }
 
 void setLED(bool state){
-  digitalWrite(pin, state);
+  digitalWrite(led_pin, state);
 }
