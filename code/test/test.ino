73cb2ea288c892e3cadc31edfd697a2ec95cdaa3
22236bhs <22236@burnside.school.nz>
2025-09-30 15:40:39 +1300

    changed pin values and added a power pin for the reed switch

--- code/test/test.ino
+++ code/test/test.ino
@@ -1,13 +1,16 @@
 //Variable Declarations
 
-int button_pin = 36;
-int led_pin = 32;
+int button_pin = 25;
+int led_pin = 33;
+int button_power_pin = 26;
 
 
 void setup(){
   Serial.begin(9600);
   pinMode(button_pin, INPUT);
   pinMode(led_pin, OUTPUT);
+  pinMode(button_power_pin, OUTPUT);
+  digitalWrite(button_power_pin, HIGH);
   digitalWrite(led_pin, LOW);
 }
  
